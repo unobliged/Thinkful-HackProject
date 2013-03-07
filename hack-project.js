5a2494c3c5407e9699d2ac9b43fd21be0b417867
unobliged <brian.ortega@gmail.com>
2013-03-07 00:50:10 -0500

    Fixed shuffling and added sliding

--- hack-project.js
+++ hack-project.js
@@ -5,13 +5,15 @@ http://developer.yahoo.com/yql/console/
 */
 
 	var yql_query = "http://query.yahooapis.com/v1/public/yql?q=SELECT%20href%20from%20html%20WHERE%20url%3D%22http%3A%2F%2Fimgur.com%2Fr%2Faww%2Ftop%22%20and%20xpath%3D'%2F%2Fdiv%5B%40class%3D%22post%22%5D%2Fa'&format=json&callback=cbfunc";
-	
+	// raww: pics on one page, rawwr: one random pic	
 	var raww = [];
 	var rawwr = '';
+	// board stores the tile sequence, _xy is for coordinates
 	var board = [];
 	var board_xy = [];
 	// will add option later for harder difficulty (more tiles)
 	var tile_count = 3;
+	var canvas, image, tile_width, tile_height;
 
 	// initializing board tile positions
 	for(var i = 0; i < tile_count*tile_count; i++){
@@ -24,7 +26,10 @@ http://developer.yahoo.com/yql/console/
 			
 			// alert(i+j + ' : ' + board_xy[i+j].x + ', ' + board_xy[i+j].y);
 		}		
-	} 
+	}
+	
+	//blank_tile will hold x and y coordinates
+	var blank_tile = {};	
 
 	$.ajax({
 		url: yql_query,
@@ -32,7 +37,7 @@ http://developer.yahoo.com/yql/console/
 		jsonpCallback: 'cbfunc',
 		error: function(){ alert('Failed!'); },
 		success: function(obj){
-			/* This gives me an array of hrefs and picks a random one */
+			// This gives me an array of hrefs and picks a random one
 			for (var x in obj.query['results']['a']){
 				raww[x] = obj.query['results']['a'][x]['href'];		
 			}			
@@ -46,18 +51,21 @@ http://developer.yahoo.com/yql/console/
 				dataType: 'json',				
 				success: function(obj){						
 					$('#image h1').text(obj.data['title']);				
-					$('#image a').attr('href', obj.data['link']);					 // hrefs are in the following format: /r/aww/fhWYwqW
+					$('#image a').attr('href', obj.data['link']);					 
+					// hrefs are in the following format: /r/aww/fhWYwqW
 					var lg_img = obj.data['link'].slice(0,-4) + 'l' + obj.data['link'].slice(-4);
 					$('#image img').attr('src', lg_img);
 				},
 				// This takes care of setting up the puzzle
 				complete: function(){					
 					$('#image img').load(function(){
-						var image = document.getElementById('test');
+						image = document.getElementById('test');
 						$('canvas').attr('width', image.width).attr('height', image.height);
-						var canvas = document.getElementById('n-puzzle').getContext('2d');
+						canvas = document.getElementById('n-puzzle').getContext('2d');
 						// canvas.drawImage(image, 0, 0, image.width, image.height);						
-						draw_puzzle(image, canvas);		
+						draw_puzzle(image, canvas);
+						
+						
 					});					
 				},
 				error: function(){ alert('Failed!'); },
@@ -67,24 +75,80 @@ http://developer.yahoo.com/yql/console/
 			});
 		}
 	});
+	var board_state = [];
+	
+	$('canvas').click(function(e){
+		e.preventDefault();		
+		
+		var mouse = {			
+			tile_num: Math.floor((e.pageX - $(this).offset().left) / tile_width) + tile_count * Math.floor((e.pageY - $(this).offset().top) / tile_height),
+			tile_x: Math.floor((e.pageX - $(this).offset().left) / tile_width) * tile_width,
+			tile_y: Math.floor((e.pageY - $(this).offset().top) / tile_height) * tile_height,
+			x: Math.floor((e.pageX - $(this).offset().left) / tile_width),
+			y: Math.floor((e.pageY - $(this).offset().top) / tile_height)			
+		};
+		
+		move_tile(mouse);			
+	});
+	
+	function move_tile(mouse){		
+		if(((mouse.x == blank_tile.x + 1 || mouse.x == blank_tile.x -1) && mouse.y == blank_tile.y) || ((mouse.y == blank_tile.y + 1 || mouse.y == blank_tile.y -1) && mouse.x == blank_tile.x)){
+			
+			board_state[blank_tile.tile_num].tile_x = board_state[mouse.tile_num].tile_x;
+			board_state[blank_tile.tile_num].tile_y = board_state[mouse.tile_num].tile_y;
+			board_state[blank_tile.tile_num].tile_num = board_state[mouse.tile_num].tile_num;
+			
+			blank_tile = mouse;			
+			
+			canvas.clearRect(0,0, image.width, image.height);
+			for(var i = 0; i < board.length; i++){
+				//alert('test');
+				if(i !== mouse.tile_num){
+					canvas.drawImage(image, board_state[i].tile_x, board_state[i].tile_y, tile_width, tile_height, board_state[i].x, board_state[i].y, tile_width, tile_height);
+				}				
+			}			
+		}		
+	}
 	
 	function draw_puzzle(image, canvas){
-		var tile_width = image.width / tile_count;
-		var tile_height = image.height / tile_count;
+		tile_width = image.width / tile_count;
+		tile_height = image.height / tile_count;
 		
 		shuffle(board);
 				
 		while(!solvable(board)){
 			shuffle(board);
 		}		
-			
-		// canvas.clearRect(0,0, image.width, image.height);
+		
 		for(var i = 0; i < board.length-1; i++){
-			canvas.drawImage(image, board_xy[i].x * tile_width, board_xy[i].y * tile_height, tile_width, tile_height, board_xy[board[i]].x * tile_width, board_xy[board[i]].y * tile_height, tile_width, tile_height);
+			canvas.drawImage(image, board_xy[board[i]].x * tile_width, board_xy[board[i]].y * tile_height, tile_width, tile_height, board_xy[i].x * tile_width, board_xy[i].y * tile_height, tile_width, tile_height);			
 			
-			// alert(i + ' -> ' + board[i] + ' :: ' + board_xy[i].x + ', ' + board_xy[i].y + ' -> ' + board_xy[board[i]].x + ', ' + board_xy[board[i]].y);
+			board_state[i] = {
+				tile_num: board[i],
+				tile_x: board_xy[board[i]].x * tile_width,
+				tile_y: board_xy[board[i]].y * tile_height,
+				x: board_xy[i].x * tile_width,
+				y: board_xy[i].y * tile_height
+			};
 			
-		}		
+		}
+		
+		board_state[board.length-1] = {
+			tile_num: board.length-1,
+			tile_x: board_xy[board.length-1].x * tile_width,
+			tile_y: board_xy[board.length-1].y * tile_height,
+			x: board_xy[board.length-1].x * tile_width,
+			y: board_xy[board.length-1].y * tile_height
+		};		
+		
+		blank_tile = {
+			// bottom right tile is the default blank tile
+			tile_num: tile_count*tile_count-1,
+			tile_x: (tile_count-1)*tile_width,
+			tile_y: (tile_count-1)*tile_height,
+			x: board_xy[tile_count*tile_count-1].x,
+			y: board_xy[tile_count*tile_count-1].y			
+		};
 	}
 	
 /* SO helped me discover this method for random sorting an array in js, which is much better than my previous usage of passing a function to sort with 0.5-rand to take advantage of the -1/0/1 accepted returns
@@ -121,6 +185,5 @@ My function is probably not that efficient, but this is for small grids so it sh
 			return true;
 		}
 	}
-	
 	
 });
